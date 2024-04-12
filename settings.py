08013136ff9938efd5524b11484c06efe5a4a8f0
ReneGRomCodes <rene.romero.2517@gmail.com>
2024-04-12 11:06:05 +0200

    Implemented 'bullet.py' and expanded 'alien_invasion.py', 'game_functions.py' and 'settings.py' to allow the ship to fire bullets

--- settings.py
+++ settings.py
@@ -3,7 +3,6 @@ class Settings():
 
     def __init__(self):
         """Initialize the game's settings."""
-
         # Screen settings.
         self.screen_width = 1200
         self.screen_height = 800
@@ -11,3 +10,9 @@ class Settings():
 
         # Ship settings.
         self.ship_speed_factor = 1.5
+
+        # Bullet settings.
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
